9b76dde591b8ae410531bf7100e6fcdcf045c9ce
Steven Welden <swelden7@gmail.com>
2022-04-26 20:08:58 -0700

    Add svg icons

---
+++ components/icons/MoonIcon.tsx
@@ -0,0 +1,12 @@
+const MoonIcon = () => (
+  <svg
+    className="h-4 w-4"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+  </svg>
+);
+
+export default MoonIcon;
---
+++ components/icons/index.tsx
@@ -0,0 +1,6 @@
+export { default as MoonIcon } from "./MoonIcon";
+export { default as SunIcon } from "./SunIcon";
+export { default as SearchIcon } from "./SearchIcon";
+export { default as CloseIcon } from "./CloseIcon";
+export { default as FilterIcon } from "./FilterIcon";
+export { default as ReverseIcon } from "./ReverseIcon";
