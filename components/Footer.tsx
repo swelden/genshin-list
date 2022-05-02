0e572cdb9323b4fd57695cfac5a0dcae2c1b709f
Steven Welden <swelden7@gmail.com>
2022-05-01 18:48:06 -0700

    Add more padding to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="container mt-auto flex items-center justify-center border-t px-4 py-3 text-center">
+    <footer className="container mt-auto flex items-center justify-center border-t px-4 py-6 text-center">
       <p>
         <strong>Genshin List</strong> is not affiliated with or endorsed by
         miHoYo.
