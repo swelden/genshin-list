2950af57963b537237a43bb1e9834b045fff92ac
Steven Welden <swelden7@gmail.com>
2022-05-17 02:05:20 -0700

    Update section padding

--- components/Sections/Section.tsx
+++ components/Sections/Section.tsx
@@ -6,7 +6,7 @@ const Section: React.FC<{
   className?: string;
 }> = ({ children, title, className }) => (
   <section
-    className={`flex flex-col gap-3 rounded-lg bg-white p-3 ring-1 ring-black/5 dark:bg-zinc-800 dark:ring-white/5 lg:p-5 ${className}`}
+    className={`flex flex-col gap-3 rounded-lg bg-white p-4 ring-1 ring-black/5 dark:bg-zinc-800 dark:ring-white/5 lg:p-5 ${className}`}
   >
     <h2 className="text-2xl">{title}</h2>
     {children}
