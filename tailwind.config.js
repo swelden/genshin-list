9b7ea747674f0818070590c160510e0e502445a2
Steven Welden <swelden7@gmail.com>
2022-05-18 15:47:06 -0700

    Fix Tailwind class generation in utils directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "class",
   theme: {
