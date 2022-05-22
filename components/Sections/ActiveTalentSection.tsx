86305ca68f6b973ed7828bfbb1254997fadbfe7c
Steven Welden <swelden7@gmail.com>
2022-05-21 20:00:17 -0700

    Extract table component

--- components/Sections/ActiveTalentSection.tsx
+++ components/Sections/ActiveTalentSection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Props } from "../../pages/[name]";
 import { DropDownIcon } from "../icons";
+import StatsTable from "../StatsTable";
 import Section from "./Section";
 import SectionRow from "./SectionRow";
 
@@ -19,18 +20,11 @@ const ActiveTalentSection: React.FC<Talents> = ({ talents }) => {
   );
 };
 
-const MaxLvlMap: { [key: string]: number } = {
-  "Normal Attack": 11,
-  "Elemental Skill": 14,
-  "Elemental Burst": 14,
-  "Alternate Sprint": 1,
-} as const;
-
 const ActiveTalentAttributes: React.FC<{
   talent: Talents["talents"]["actives"][number];
 }> = ({ talent }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const maxLvl = talent.attributes[0].params.length;
+  const maxCols = talent.attributes[0].params.length;
 
   return (
     <div>
@@ -40,64 +34,9 @@ const ActiveTalentAttributes: React.FC<{
           <DropDownIcon />
         </span>
       </button>
-      {isOpen && (
-        <div
-          className={`scrollbar mt-4 w-full overflow-x-auto rounded-lg border ${sharedBorderClasses}`}
-        >
-          <div
-            className={`overflow-hidden ${maxLvl === 1 ? "w-auto" : "w-max"}`}
-          >
-            <table className="w-full text-left text-sm">
-              <tbody>
-                <tr className={`border-b ${sharedBorderClasses}`}>
-                  <TableHeading>Level</TableHeading>
-                  {Array.from(Array(maxLvl).keys()).map((i) => (
-                    <TableHeading key={i}>{`Lv.${i + 1}`}</TableHeading>
-                  ))}
-                </tr>
-                {talent.attributes.map(({ label, params }) => {
-                  // const [heading, params] = label.split("|");
-
-                  return (
-                    <tr
-                      key={label}
-                      className={`border-b last:border-0 ${sharedBorderClasses}`}
-                    >
-                      <TableHeading>{label}</TableHeading>
-                      {params.map((param, i) => (
-                        <TableCell key={i}>{param}</TableCell>
-                      ))}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
+      {isOpen && <StatsTable talent={talent} numCols={maxCols} />}
     </div>
   );
 };
-
-const sharedBorderClasses = "border-black/10 dark:border-white/10";
-const sharedCellClasses = "border-r p-3 last:border-r-0";
-
-const TableHeading: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => (
-  <th
-    className={`w-36 bg-zinc-100 font-normal text-black/50 dark:bg-zinc-900/50 dark:text-white/30 ${sharedBorderClasses} ${sharedCellClasses}`}
-  >
-    {children}
-  </th>
-);
-
-const TableCell: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <td
-    className={`min-w-[7rem] text-black/90 last:border-r-0 dark:text-white/90 ${sharedBorderClasses} ${sharedCellClasses}`}
-  >
-    {children}
-  </td>
-);
 
 export default ActiveTalentSection;
---
+++ components/StatsTable.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Props } from "../pages/[name]";
+
+type Talents = Pick<Props, "talents">;
+
+const StatsTable: React.FC<{
+  talent: Talents["talents"]["actives"][number];
+  numCols: number;
+}> = ({ talent, numCols }) => {
+  return (
+    <div
+      className={`scrollbar mt-4 w-full overflow-x-auto rounded-lg border ${sharedBorderClasses}`}
+    >
+      <div className={`overflow-hidden ${numCols === 1 ? "w-auto" : "w-max"}`}>
+        <table className="w-full text-left text-sm">
+          <tbody>
+            <TableRow heading="Level">
+              {Array.from(Array(numCols).keys()).map((i) => (
+                // TODO: add class to change width of top row heading
+                <TableHeading key={i}>{`Lv.${i + 1}`}</TableHeading>
+              ))}
+            </TableRow>
+            {talent.attributes.map(({ label, params }) => {
+              return (
+                <TableRow heading={label} key={label}>
+                  {params.map((param, i) => (
+                    <TableCell key={i}>{param}</TableCell>
+                  ))}
+                </TableRow>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+const sharedBorderClasses = "border-black/10 dark:border-white/10";
+const sharedCellClasses = "border-r p-3 last:border-r-0";
+
+const TableRow: React.FC<{ heading: string; children: React.ReactNode }> = ({
+  heading,
+  children,
+}) => (
+  <tr className={`border-b last:border-0 ${sharedBorderClasses}`}>
+    <TableHeading>{heading}</TableHeading>
+    {children}
+  </tr>
+);
+
+const TableHeading: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <th
+    className={`w-36 bg-zinc-100 font-normal text-black/50 dark:bg-zinc-900/50 dark:text-white/30 ${sharedBorderClasses} ${sharedCellClasses}`}
+  >
+    {children}
+  </th>
+);
+
+const TableCell: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <td
+    className={`min-w-[7rem] text-black/90 last:border-r-0 dark:text-white/90 ${sharedBorderClasses} ${sharedCellClasses}`}
+  >
+    {children}
+  </td>
+);
+
+export default StatsTable;
