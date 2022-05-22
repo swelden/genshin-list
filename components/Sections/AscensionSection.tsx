4b088872827f569c276ec757da032088a3673443
Steven Welden <swelden7@gmail.com>
2022-05-22 00:41:54 -0700

    Add stats table to ascension section

--- components/Sections/AscensionSection.tsx
+++ components/Sections/AscensionSection.tsx
@@ -1,14 +1,18 @@
 import { Props } from "../../pages/[name]";
+import StatsTable from "../StatsTable";
 import Section from "./Section";
 
-// TODO: add stats
 const AscensionSection: React.FC<Pick<Props, "ascensions">> = ({
   ascensions,
 }) => {
   console.log(ascensions);
   return (
-    <Section title="Ascensions">
-      <div>Ascensions</div>
+    <Section title="Ascensions" className="overflow-hidden">
+      <StatsTable
+        data={ascensions.stats.data}
+        numCols={ascensions.stats.headings.length}
+        topHeadings={ascensions.stats.headings}
+      />
     </Section>
   );
 };
