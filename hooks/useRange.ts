1cc40917f412f30b34fbf6a680ed7f360f03db53
Steven Welden <swelden7@gmail.com>
2022-05-24 22:12:29 -0700

    Add step and gap to useRange

--- hooks/useRange.ts
+++ hooks/useRange.ts
@@ -3,6 +3,8 @@ import { useState } from "react";
 const useRange = (
   min: number,
   max: number,
+  step: number = 1,
+  gap: number = 1,
   initialStart: number = min,
   initialEnd: number = max
 ) => {
@@ -10,19 +12,19 @@ const useRange = (
   const [end, setEnd] = useState(initialEnd);
 
   const decrementStart = () => {
-    setStart(Math.max(start - 1, min));
+    setStart(Math.max(start - step, min));
   };
 
   const incrementStart = () => {
-    setStart(Math.min(start + 1, end - 1));
+    setStart(Math.min(start + step, end - gap));
   };
 
   const decrementEnd = () => {
-    setEnd(Math.max(end - 1, start + 1));
+    setEnd(Math.max(end - step, start + gap));
   };
 
   const incrementEnd = () => {
-    setEnd(Math.min(end + 1, max));
+    setEnd(Math.min(end + step, max));
   };
 
   const reset = () => {
