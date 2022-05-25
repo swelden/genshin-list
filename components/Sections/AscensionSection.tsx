8d739c21c6e57c4b42e1564dc06c827702f27a12
Steven Welden <swelden7@gmail.com>
2022-05-24 22:17:42 -0700

    Add double range slider

--- components/Sections/AscensionSection.tsx
+++ components/Sections/AscensionSection.tsx
@@ -1,5 +1,6 @@
-import useRange from "../../hooks/useRange";
+import { useCallback, useState } from "react";
 import { Ascensions, MaterialDataMap } from "../../pages/[name]";
+import DoubleRangeSlider from "../DoubleRangeSlider/DoubleRangeSlider";
 import MaterialList, { calculateMaterialsRange } from "../MaterialList";
 import StatsTable from "../StatsTable";
 import Section from "./Section";
@@ -27,39 +28,28 @@ const AscensionCalculator: React.FC<{
   costs: Ascensions["costs"];
   materialData: MaterialDataMap;
 }> = ({ costs, materialData }) => {
-  const {
-    start,
-    decrementStart,
-    incrementStart,
-    end,
-    decrementEnd,
-    incrementEnd,
-    // reset,
-  } = useRange(0, 6);
+  const [totalMaterials, setTotalMaterials] = useState({});
 
-  const totalMaterials = calculateMaterialsRange(costs, start, end);
-  console.log(totalMaterials);
+  const updateMaterials = useCallback(
+    (start: number, end: number) => {
+      setTotalMaterials(calculateMaterialsRange(costs, start, end));
+    },
+    [costs]
+  );
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-4">
       <h3>Material Calculator</h3>
-      <div className="flex items-center gap-4 text-gold">
-        <h4>Start {start}</h4>
-        <button className="p-2" onClick={decrementStart}>
-          -
-        </button>
-        <button className="p-2" onClick={incrementStart}>
-          +
-        </button>
-      </div>
-      <div className="flex items-center gap-4 text-gold">
-        <h4>End {end}</h4>
-        <button className="p-2" onClick={decrementEnd}>
-          -
-        </button>
-        <button className="p-2" onClick={incrementEnd}>
-          +
-        </button>
-      </div>
+      <DoubleRangeSlider
+        initialMin={0}
+        initialMax={6}
+        min={0}
+        max={6}
+        step={1}
+        gap={1}
+        onChange={updateMaterials}
+      />
+
       <MaterialList
         totalMaterials={totalMaterials}
         materialData={materialData}
