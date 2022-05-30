9ccf5a9f248c622bd378c75d3c73ff9bdc5aa1ec
Steven Welden <swelden7@gmail.com>
2022-05-30 02:48:29 -0700

    Change miHoYo to HoYoverse

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="container mt-auto flex items-center justify-center border-t px-4 py-6 text-center">
-      <p>
+    <footer className="container mt-auto flex items-center justify-center border-t border-zinc-400 px-4 py-6 text-center dark:border-zinc-500">
+      <p className="text-zinc-700 dark:text-zinc-200">
         <strong>Genshin List</strong> is not affiliated with or endorsed by
-        miHoYo.
+        HoYoverse.
       </p>
     </footer>
   );
