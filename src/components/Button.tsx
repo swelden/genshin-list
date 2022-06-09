b3f525cb9aa5d6396aa5c2ab4dff122d17951444
Steven Welden <swelden7@gmail.com>
2022-06-08 19:33:08 -0700

    Update circle button hover and button focus styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,7 +22,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`flex cursor-pointer items-center justify-center rounded-full border-0 font-medium shadow-sm transition duration-75 hover:border-2 hover:shadow-inner focus-visible:border-2 focus-visible:shadow-inner active:border-opacity-70 active:bg-ui-bg-click active:text-white active:shadow-lg ${
+      className={`flex cursor-pointer items-center justify-center rounded-full border-0 font-medium shadow-sm outline-none ring-yellow-500 ring-offset-zinc-100 transition duration-75 hover:border-2 hover:shadow-inner focus-visible:border-2 focus-visible:shadow-inner focus-visible:ring-2 focus-visible:ring-offset-2 active:border-opacity-70 active:bg-ui-bg-click active:text-white active:shadow-lg dark:ring-yellow-600 dark:ring-offset-zinc-900 ${
         isCircle ? "h-8 w-8" : "h-9 w-full"
       } ${
         isColorInversed
@@ -50,12 +50,12 @@ export const CircleButton: React.FC<CircleButtonProps> = ({
       className="group relative flex h-10 w-10 cursor-pointer items-center justify-center rounded-full"
     >
       <Button
-        className={`peer z-[1] group-hover:border-2 group-hover:shadow-inner ${className}`}
+        className={`peer z-[1] group-hover:h-9 group-hover:w-9 group-hover:border-2 group-hover:shadow-inner ${className}`}
         isCircle={true}
       >
         {children}
       </Button>
-      <div className="absolute top-1/2 left-1/2 z-0 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-ui-outline transition-[height,_width] duration-200 group-hover:h-7 group-hover:w-7 peer-focus-visible:h-7 peer-focus-visible:w-7"></div>
+      <div className="absolute top-1/2 left-1/2 z-0 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-ui-outline transition-[height,_width] duration-300 hover:transition-none group-hover:h-8 group-hover:w-8 peer-focus-visible:h-8 peer-focus-visible:w-8"></div>
     </div>
   );
 };
