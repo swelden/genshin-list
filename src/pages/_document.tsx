cfe359320117b729754b12ab994c95b1668bf402
Steven Welden <swelden7@gmail.com>
2022-06-11 03:17:10 -0700

    Optimize font with preload link and display swap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/ja-jp.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
       {/* BUG: transition-colors does not work on linear gradient so removing it for now */}
       <body className="bg-zinc-100 dark:bg-zinc-900 dark:text-white">
         <Main />
