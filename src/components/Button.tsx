afbb39065e3b41d3538c6a52d876c2740d2bd2e0
Steven Welden <swelden7@gmail.com>
2022-06-10 22:07:56 -0700

    Update circle button hover animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -32,7 +32,7 @@ const Button = React.forwardRef<React.ElementType, ButtonProps>(
       <ButtonType
         ref={ref}
         onClick={onClick}
-        className={`flex cursor-pointer items-center justify-center rounded-full border-0 shadow-sm transition duration-75 hover:border-2 hover:shadow-inner focus-visible:border-2 focus-visible:shadow-inner active:border-opacity-70 active:bg-ui-bg-click active:text-white active:shadow-lg ${
+        className={`flex cursor-pointer items-center justify-center rounded-full border-0 shadow-sm transition duration-75 hover:border-2 hover:shadow-inner focus-visible:border-2 focus-visible:shadow-inner active:border-opacity-70 active:bg-ui-bg-click active:text-white active:shadow-lg dark:active:bg-ui-bg-click dark:active:text-white ${
           isCircle ? "h-8 w-8" : "h-9 w-full"
         } ${as === "button" ? "key-focus key-focus-body" : ""} ${
           isColorInversed
@@ -74,7 +74,6 @@ export const LinkButton: React.FC<LinkButtonProps> = ({
 
 type CircleButtonProps = Omit<ButtonProps, "isCircle" | "as">;
 
-// TODO: I think only the inside circle changes size (no transition grows to fill outline circle then transitions back to original size)
 export const CircleButton: React.FC<CircleButtonProps> = ({
   children,
   onClick,
@@ -88,7 +87,7 @@ export const CircleButton: React.FC<CircleButtonProps> = ({
       className="key-focus key-focus-body group relative flex h-10 w-10 cursor-pointer items-center justify-center rounded-full"
     >
       <Button
-        className={`peer z-[1] group-hover:h-9 group-hover:w-9 group-hover:border-2 group-hover:shadow-inner ${className}`}
+        className={`peer z-[1] transition-[height,_width] duration-300 group-hover:h-10 group-hover:w-10 group-hover:border-2 group-hover:shadow-inner group-hover:transition-none ${className}`}
         isCircle={true}
         ariaHaspopup={ariaHaspopup}
         ariaExpanded={ariaExpanded}
@@ -96,7 +95,7 @@ export const CircleButton: React.FC<CircleButtonProps> = ({
       >
         {children}
       </Button>
-      <div className="absolute top-1/2 left-1/2 z-0 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-ui-outline transition-[height,_width] duration-300 hover:transition-none group-hover:h-8 group-hover:w-8 peer-focus-visible:h-8 peer-focus-visible:w-8"></div>
+      <div className="absolute top-1/2 left-1/2 z-0 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-ui-outline"></div>
     </button>
   );
 };
