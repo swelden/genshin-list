69ab0ff4afc25930841d4fa9c44a6c680449e041
Steven Welden <swelden7@gmail.com>
2022-06-18 21:35:20 -0700

    Change focus ring to blue on light mode

--- src/components/Filters/SearchFilter.tsx
+++ src/components/Filters/SearchFilter.tsx
@@ -21,7 +21,7 @@ const Search: SearchProps = ({ setFilter }) => {
   };
 
   return (
-    <div className="flex items-center rounded-full border-2 border-zinc-300 transition-colors ease-in-out focus-within:border-yellow-500 dark:border-btn-navy dark:focus-within:border-yellow-600">
+    <div className="flex items-center rounded-full border-2 border-zinc-300 transition-colors ease-in-out focus-within:border-blue-600 dark:border-btn-navy dark:focus-within:border-yellow-600">
       <div className="flex w-10 justify-center text-btn-navy dark:text-zinc-300">
         <SearchIcon />
       </div>
