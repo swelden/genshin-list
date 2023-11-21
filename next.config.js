3f2edaf9a947d30e49074e6a47cdc192045f353b
Steven Welden <swelden7@gmail.com>
2023-11-21 07:02:24 -0800

    Remove port from config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        port: "",
         pathname: "/genshin/image/upload/**",
       },
       {
         protocol: "https",
         hostname: "api.ambr.top",
-        port: "",
         pathname: "/assets/**",
       },
     ],
