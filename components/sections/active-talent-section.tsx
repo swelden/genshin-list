bf17afff7f8d7ae5ccafb8208153219072f2df57
Steven Welden <swelden7@gmail.com>
2023-11-30 07:24:11 -0800

    Refactor ActiveTalentAttributes using Collapsible

--- components/sections/active-talent-section.tsx
+++ components/sections/active-talent-section.tsx
@@ -4,16 +4,21 @@ import * as React from "react";
 
 import { TalentInfo } from "@/lib/get-character-details";
 import { cn } from "@/lib/utils";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { Section, SectionHeader } from "@/components/ui/section";
 import { Icons } from "@/components/icons";
 import { SectionRow } from "@/components/sections/section-row";
 import { StatsTable } from "@/components/stats-table";
 
-export const ActiveTalentSection = ({
-  actives,
-}: {
+interface ActiveTalentSectionProps {
   actives: TalentInfo["actives"];
-}) => {
+}
+
+export const ActiveTalentSection = ({ actives }: ActiveTalentSectionProps) => {
   return (
     <Section className="overflow-hidden">
       <SectionHeader>Active Talents</SectionHeader>
@@ -27,42 +32,35 @@ export const ActiveTalentSection = ({
 };
 ActiveTalentSection.displayName = "ActiveTalentSection";
 
-// TODO: replace with @radix-ui/react-collapsible
-const ActiveTalentAttributes = ({
-  talent,
-}: {
+interface ActiveTalentAttributesProps {
   talent: TalentInfo["actives"][number];
-}) => {
+}
+
+// TODO: extract this to separate file due to "use-client"
+const ActiveTalentAttributes = ({ talent }: ActiveTalentAttributesProps) => {
   const [isOpen, setIsOpen] = React.useState(false);
   const maxCols = talent.attributes[0].params.length;
-  const panelId = `${talent.category.toLowerCase().replace(/\s/g, "-")}-panel`;
 
   return (
-    <div className="mt-4">
-      <button
-        className="flex max-w-max rounded-full bg-zinc-100 p-2 pl-4 pr-2 ring-1 ring-black/5 transition-colors hover:bg-zinc-200 dark:bg-zinc-700 hover:dark:bg-zinc-600"
-        onClick={() => setIsOpen(!isOpen)}
-        aria-expanded={isOpen}
-        aria-controls={isOpen ? panelId : undefined}
-      >
-        <span className="pr-1.5">{talent.category} Stats</span>
-        <span className={cn("transition-transform", isOpen && "rotate-180")}>
-          <Icons.dropdown className="h-6 w-6 min-w-[1.5rem]" />
-        </span>
-      </button>
-
-      {isOpen && (
-        <div id={panelId}>
-          <StatsTable
-            data={talent.attributes}
-            numCols={maxCols}
-            topHeadings={Array.from(Array(maxCols).keys()).map(
-              (lvl) => `Lv.${lvl + 1}`,
-            )}
-          />
-        </div>
-      )}
-    </div>
+    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="mt-4">
+      <CollapsibleTrigger asChild>
+        <button className="flex max-w-max rounded-full bg-zinc-100 p-2 pl-4 pr-2 ring-1 ring-black/5 transition-colors hover:bg-zinc-200 dark:bg-zinc-700 hover:dark:bg-zinc-600">
+          <span className="pr-1.5">{talent.category} Stats</span>
+          <span className={cn("transition-transform", isOpen && "rotate-180")}>
+            <Icons.dropdown className="h-6 w-6 min-w-[1.5rem]" />
+          </span>
+        </button>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <StatsTable
+          data={talent.attributes}
+          numCols={maxCols}
+          topHeadings={Array.from(Array(maxCols).keys()).map(
+            (lvl) => `Lv.${lvl + 1}`,
+          )}
+        />
+      </CollapsibleContent>
+    </Collapsible>
   );
 };
 ActiveTalentAttributes.displayName = "ActiveTalentAttributes";
