703e07328cdc6a41a6a91e774731cfbc4b85f8a8
Steven Welden <swelden7@gmail.com>
2023-12-01 05:36:08 -0800

    Add JSDoc annotations to provide type information

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-import plugin from "tailwindcss/plugin";
+const { plugin } = require("tailwindcss/plugin");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
