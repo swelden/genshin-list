13cb143dbdc2add41c8976c5b5228bf3c503c0fd
Steven Welden <swelden7@gmail.com>
2023-12-10 02:51:58 -0800

    Move collapsible open logic to new component

--- components/sections/active-talent-section.tsx
+++ components/sections/active-talent-section.tsx
@@ -1,13 +1,8 @@
-"use client";
-
-import * as React from "react";
-
 import type { Active } from "@/data/types";
-import { cn } from "@/lib/utils";
 import {
-  Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
+  CollapsibleWithState,
 } from "@/components/ui/collapsible";
 import { Section, SectionHeader } from "@/components/ui/section";
 import { Icons } from "@/components/icons";
@@ -35,29 +30,25 @@ interface ActiveTalentAttributesProps {
   talent: Active;
 }
 
-// TODO: extract this to separate file due to "use-client"
 function ActiveTalentAttributes({ talent }: ActiveTalentAttributesProps) {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const maxCols = talent.attributes[0]?.params.length ?? 1;
+  const numCols = talent.attributes[0]!.params.length;
 
   return (
-    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="mt-4">
+    <CollapsibleWithState className="group mt-4">
       <CollapsibleTrigger asChild>
         <button className="flex max-w-max rounded-full bg-zinc-100 p-2 pl-4 ring-1 ring-black/5 transition-colors hover:bg-zinc-200 dark:bg-zinc-700 hover:dark:bg-zinc-600">
           <span className="pr-1.5">{talent.category} Stats</span>
-          <span className={cn("transition-transform", isOpen && "rotate-180")}>
-            <Icons.dropdown className="h-6 w-6 min-w-[1.5rem]" />
-          </span>
+          <Icons.dropdown className="h-6 w-6 min-w-[1.5rem] transition-transform group-data-[state=open]:rotate-180" />
         </button>
       </CollapsibleTrigger>
       <CollapsibleContent className="mt-4">
         <StatsTable
           data={talent.attributes}
-          topHeadings={Array.from(Array(maxCols).keys()).map(
+          topHeadings={Array.from(Array(numCols).keys()).map(
             (lvl) => `Lv.${lvl + 1}`,
           )}
         />
       </CollapsibleContent>
-    </Collapsible>
+    </CollapsibleWithState>
   );
 }
--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 
 const Collapsible = CollapsiblePrimitive.Root;
@@ -8,4 +9,30 @@ const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
 
 const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
 
-export { Collapsible, CollapsibleContent, CollapsibleTrigger };
+const CollapsibleWithState = React.forwardRef<
+  React.ElementRef<typeof Collapsible>,
+  Omit<
+    React.ComponentPropsWithoutRef<typeof Collapsible>,
+    "open" | "onOpenChange"
+  >
+>(({ className, ...props }, ref) => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Collapsible
+      open={open}
+      onOpenChange={setOpen}
+      className={className}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+CollapsibleWithState.displayName = "CollapsibleWithState";
+
+export {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  CollapsibleWithState,
+};
