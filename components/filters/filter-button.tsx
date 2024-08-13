986a1bc7d7993d2b79be1fcfa71a67204c138d07
Steven Welden <swelden7@gmail.com>
2024-08-12 20:10:57 -0700

    Add role checkbox to FilterButton

--- components/filters/filter-button.tsx
+++ components/filters/filter-button.tsx
@@ -25,7 +25,7 @@ export function FilterButton({
     setAttrFilter({ ...attrFilter, [category]: newSet });
   };
 
-  const isSelected = (attrFilter[category] as Set<FilterAttribute>).has(attr);
+  const isChecked = (attrFilter[category] as Set<FilterAttribute>).has(attr);
 
   return (
     <button
@@ -33,23 +33,25 @@ export function FilterButton({
       onClick={() => handleFilter(attr)}
       className={cn(
         "flex h-12 flex-nowrap items-center gap-3 overflow-hidden whitespace-nowrap border-2 border-sheet-btn-border px-3 text-left text-base text-genshin-brown outline-none ring-offset-secondary transition active:!ring-0 hocus:ring-4 hocus:ring-white md:h-16 md:text-xl",
-        isSelected &&
+        isChecked &&
           "border-sheet-btn-border-selected bg-genshin-brown text-genshin-blue ring-2 ring-genshin-brown",
       )}
+      role="checkbox"
+      aria-checked={isChecked}
     >
       {/* need to position absolute to transition border color on circle */}
       <div className="relative hidden aspect-square size-7 md:block">
         <Check
           className={cn(
             "absolute inset-0 size-full text-icon-green",
-            !isSelected && "hidden",
+            !isChecked && "hidden",
           )}
           strokeWidth="3"
         />
         <div
           className={cn(
             "absolute inset-0 size-full rounded-full border-2 border-sheet-btn-border transition-colors",
-            isSelected && "size-0 scale-0 border-genshin-brown",
+            isChecked && "size-0 scale-0 border-genshin-brown",
           )}
         />
       </div>
